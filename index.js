33507917c3775f43db366d3ccda7c6408522741e
MasudMahamud <mahamud.al.msdud@gmail.com>
2021-10-24 02:45:11 +0600

    blog dynamic

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n50q4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('blog'))
+app.use(fileUpload());
 
 const port = 4000;
 
 
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const adminCollection = client.db("volunteerNetwork").collection("admin");
     const activityCollection = client.db("volunteerNetwork").collection("activity");
     const volunteersCollection = client.db("volunteerNetwork").collection("volunteer");
+    const blogCollection = client.db("volunteerNetwork").collection("blog");
 
     //activity 
     app.get('/activity', (req, res) => {
@@ -28,7 +29,9 @@ client.connect(err => {
             .toArray((err, documents) => {
                 res.send(documents);
             })
-    })
+    }) //activity end
+
+
     //volunteer
     app.post('/addVolunteer', (req, res) => {
         const volunteer = req.body;
@@ -45,24 +48,58 @@ client.connect(err => {
             })
     })
 
+    app.get('/allVolunteer', (req, res) => {
+        volunteersCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
     app.delete('deleteEvent/:id', (req, res) => {
         const id = ObjectID(req.params.id);
         volunteersCollection.findOneAndDelete({ _id: id })
             .then(documents => res.send(!!documents.value))
-    })
+    }) //volunteer end
 
-//admin
+
+    //admin
     app.post('/isAdmin', (req, res) => {
         const email = req.body.email;
         adminCollection.find({ email: email })
             .toArray((err, doctors) => {
                 res.send(doctors.length > 0);
             })
+    }) //admin end
+
+    
+    //blog
+    app.post('/addBlog', (req, res) => {
+        const file = req.files.file;
+        const title = req.body.title;
+        const date = req.body.date;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        blogCollection.insertOne({ title, date, image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
     })
 
+    app.get('/blog', (req, res) => {
+        blogCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    }); //blog end
 
 });
-
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
